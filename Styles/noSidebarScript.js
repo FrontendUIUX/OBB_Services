574c16a8568c75dcde6a7f327fb0afa6ec3b413b
Silvana Cheaib <ISS.Design.Team@gmail.com>
2025-07-16 14:32:14 +0300

    Create noSidebarScript.js

---
+++ Styles/noSidebarScript.js
@@ -0,0 +1,6 @@
+  window.addEventListener('DOMContentLoaded', function () {
+    var spanExists = document.getElementById('6059be75-622f-41eb-8fcf-fd017ab9ff53');
+    if (!spanExists) {
+      document.body.classList.add('m-0');
+    }
+  });
