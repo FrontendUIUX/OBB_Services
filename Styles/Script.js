942cf91d9b56335448f747c690744117c1155429
FrontendUIUX <ISS.Design.Team@gmail.com>
2025-07-16 14:27:07 +0300

    Update Script.js

--- Styles/Script.js
+++ Styles/Script.js
@@ -70,3 +70,9 @@ $(document).ready(function(){
         $("[name='btnOutgoing']").removeClass("active");
     })
 })
+  window.addEventListener('DOMContentLoaded', function () {
+    var spanExists = document.getElementById('6059be75-622f-41eb-8fcf-fd017ab9ff53');
+    if (!spanExists) {
+      document.body.classList.add('m-0');
+    }
+  });
