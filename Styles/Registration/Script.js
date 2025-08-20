389987c9e8f0718e7d88a865ad73447263e4ea80
FrontendUIUX <ISS.Design.Team@gmail.com>
2025-08-20 09:03:20 +0300

    Update Script.js

--- Styles/Registration/Script.js
+++ Styles/Registration/Script.js
@@ -77,33 +77,6 @@ document.addEventListener("DOMContentLoaded", function () {
 	let menuItems;
     const currentUrl = window.location.href;
 
-$(document).ready(function () {
-    let fqn = null; // declare properly
-
-    // Try to get user FQN
-    setTimeout(function () {
-        try {
-            fqn = SourceCode.Forms.Settings.User.FQN || null;
-            console.log("Logged-in User FQN:", fqn);
-            menuBar();
-        } catch (e) {
-            console.error("Error retrieving FQN:", e);
-        }
-    }, 1000);
-
-    // Handle navbar click
-    $(".navbarBrand a").on("click", function (e) {
-        e.preventDefault(); // stop default link behavior
-        if (fqn) {
-            console.log("Interal User");
-            window.location.href = "https://ck2-app-tst-1.obc.local/Runtime/Runtime/Form/OBBHub.Form/";
-        } else {
-            console.log("External User");
-            window.location.href = "https://ck2-app-tst-1.obc.local/Runtime/Runtime/Form/OBBHubExternal.Form/";
-        }
-    });
-});
-
 
 // Call the function
 
@@ -527,4 +500,31 @@ document.addEventListener('DOMContentLoaded', function () {
       console.warn("Could not parse data-options:", dataOptions);
     }
   });
+});
+
+$(document).ready(function () {
+    let fqn = null; // declare properly
+
+    // Try to get user FQN
+    setTimeout(function () {
+        try {
+            fqn = SourceCode.Forms.Settings.User.FQN || null;
+            console.log("Logged-in User FQN:", fqn);
+            menuBar();
+        } catch (e) {
+            console.error("Error retrieving FQN:", e);
+        }
+    }, 1000);
+
+    // Handle navbar click
+    $(".navbarBrand a").on("click", function (e) {
+        e.preventDefault(); // stop default link behavior
+        if (fqn) {
+            console.log("Interal User");
+            window.location.href = "https://ck2-app-tst-1.obc.local/Runtime/Runtime/Form/OBBHub.Form/";
+        } else {
+            console.log("External User");
+            window.location.href = "https://ck2-app-tst-1.obc.local/Runtime/Runtime/Form/OBBHubExternal.Form/";
+        }
+    });
 });
