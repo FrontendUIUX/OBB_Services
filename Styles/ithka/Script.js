7097130ee2884b491c988e6a97699f76adf96886
FrontendUIUX <silvanacheaib@gmail.com>
2025-10-01 08:40:55 +0300

    Update Script.js

--- Styles/ithka/Script.js
+++ Styles/ithka/Script.js
@@ -343,6 +343,7 @@ if (currentUrl.includes("ArchivingRequest") ||  currentUrl.includes("RAP") || cu
 		{
 					text: "Outgoing Correspondence", url: "", children: [
 						{ text: "InProgress", url: "/Runtime/Runtime/Form/ITHCA.OutgoingRequest.InProgressRequestForm/" },
+                        { text: "Closed", url: "/Runtime/Runtime/Form/ITHCA.OutgoingRequest.ClosedRequestForm/" },
 						{ text: "All Requests", url: "/Runtime/Runtime/Form/ITHCA.OutgoingRequest.AllRequestsForm/" },
 						{ text: "My Tasks", url: "/Runtime/Runtime/Form/ITHCA.OutgoingRequest.WorklistForm/" },
                         { text: "Old Data", url: "/Runtime/Runtime/Form/OCMS.DataDashboard1/" }
